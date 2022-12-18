9428e7f8b43459e5a860190f656b7f89c5c923bf
YuYue20210207 <1013702038@qq.com>
2022-12-18 10:41:55 +0800

    构建项目

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import ElementPlus from 'element-plus' //引入element-plus 组件
+import 'element-plus/dist/index.css'  //引入element-plus 样式
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import './styles/main.scss'  //全局样式
+
+
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
